edc6eac85d8cbcd68b3b377bb3ac6f43b7b565a7
Novel272 <mohammedalmustafak@gmail.com>
2024-03-09 06:10:04 -0800

    f

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
-from fabric.api import *
+"""
+function for deploying web_static content to web servers.
+"""
 import os
 from datetime import datetime
-
+from fabric.api import env, put, run
+import shlex
 env.hosts = ['54.160.65.223', '54.160.65.223']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    a_list = archive_path.split(".tgz")
-    archive_wo_ext = "".join(a_list)
-    b_list = archive_wo_ext.split("versions/")
-    archive_wo_ext_ver = "".join(b_list)
-    c_list = archive_path.split("versions/")
-    archive_wo_ver = "".join(c_list)
-    if archive_path:
-        put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}/".
-            format(archive_wo_ext_ver))
-        run("tar -zxf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_wo_ver, archive_wo_ext_ver))
-        run("rm -r /tmp/{}".format(archive_wo_ver))
-        run("mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(archive_wo_ext_ver,
-                                              archive_wo_ext_ver))
-        run("rm -rf /data/web_static/releases/{}/web_static".
-            format(archive_wo_ext_ver))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(archive_wo_ext_ver))
-        print("New version deployed!")
-        return True
-    else:
+    """ Deploys archive to web servers """
+    if not os.path.exists(archive_path):
         return False
 
-
-def do_pack():
     try:
-        filepath = "versions/web_static_" + datetime.now().\
-                   strftime("%Y%m%d%H%M%S") + ".tgz"
-        local("mkdir -p versions")
-        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
-        web_static")
-        print("web_static packed: {} -> {}".
-              format(filepath, os.path.getsize(filepath)))
-    except:
-        return None
+        # Parse archive file name and web_static folder name
+        filename = os.path.basename(archive_path)
+        web_static_folder = filename.split('.')[0]
+
+        # Construct remote paths
+        releases_path = '/data/web_static/releases/{}'.format(web_static_folder)
+        tmp_path = '/tmp/{}'.format(filename)
+
+        # Upload and extract archive
+        put(archive_path, tmp_path)
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf {} -C {}'.format(tmp_path, releases_path))
+
+        # Delete temporary archive and move web_static content to releases_path
+        run('rm {}'.format(tmp_path))
+        run('mv {}/web_static/* {}'.format(releases_path, releases_path))
+
+        # Remove empty web_static folder and create symbolic link
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+
+        print('New version deployed!')
+        return True
+    except Exception as e:
+        print('Error: {}'.format(e))
+        return False
